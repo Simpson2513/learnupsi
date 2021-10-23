cf8f6792d7e45dc601ea53d30af3191769df2e39
simpson2513 <simanov2513@gmail.com>
2021-10-23 11:41:50 +0300

    creat project

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] args) {
+        boolean isGreenLight = false;
+        int speedOfPlayer1 = 5;
+        int speedOfPlayer2 = 0;
+        int speedOfPlayer3 = 2;
+        int count = 0;
+
+        if (!isGreenLight) {
+            if (speedOfPlayer1 != 0)
+                count++;
+            if (speedOfPlayer2 != 0)
+                count++;
+            if (speedOfPlayer3 != 0)
+                count++;
+        }
+        System.out.println(count + " выбывают");
+    }
+}
